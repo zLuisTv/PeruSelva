00a878162a944971df7c039fc49f071fe9a31418
zLuisTv <70212840+zLuisTv@users.noreply.github.com>
2023-07-27 06:09:39 -0500

    Create Loading.js

---
+++ scripts/Loading.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    var loaderContainer = document.getElementById('onload');
+    loaderContainer.style.display = 'none';
+    var navigation = document.getElementById('myNav');
+    navigation.style.visibility = 'visible';
+});
